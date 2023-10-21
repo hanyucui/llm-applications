238caac3a4d03d7349b2a242bb1c793e94d1d572
Hanyu Cui <hanyuc.gml@gmail.com>
2023-10-21 15:23:30 -0700

    do tee -a

--- setup-pgvector.sh
+++ setup-pgvector.sh
@@ -8,7 +8,7 @@ sudo apt update && sudo apt install -y postgresql postgresql-contrib
 sudo apt install -y postgresql-server-dev-15
 pushd /tmp && git clone --branch v0.4.4 https://github.com/pgvector/pgvector.git && pushd pgvector && make && sudo make install && popd && popd
 # Activate pgvector and the database
-echo 'ray ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers
+echo 'ray ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
 sudo service postgresql start
 # pragma: allowlist nextline secret
 sudo -u postgres psql -c "ALTER USER postgres with password 'postgres';"

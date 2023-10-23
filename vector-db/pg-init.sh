388455b2e2468831537e8a091d8d8f4006ae5400
Hanyu Cui <hanyuc.gml@gmail.com>
2023-10-22 18:10:58 -0700

    Wait at the end

--- vector-db/pg-init.sh
+++ vector-db/pg-init.sh
@@ -4,3 +4,4 @@ sudo service postgresql start
 # pragma: allowlist nextline secret
 sudo -u postgres psql -c "ALTER USER postgres with password 'postgres';"
 sudo -u postgres psql -c "CREATE EXTENSION vector;"
+sleep infinity
